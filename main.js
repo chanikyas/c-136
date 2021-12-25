07ff10b4348b8be120296a63fc93ff4a0cfca902
chanikyas <86613612+chanikyas@users.noreply.github.com>
2021-12-25 20:28:24 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status =""
+
+function setup(){
+    canvas= createCanvas(380, 380)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.size(380, 380)
+    video.hide()
+}
+
+function draw(){
+    image(video, 0, 0, 380, 380)
+}
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd",modelloded)
+    document.getElementById("status").innerHTML ="Status : Dettecting objects"
+    object_name = document.getElementById("object_name").value
+
+}
+
+function modelloded(){
+    console.log("modelloded")
+    status = true
+}
